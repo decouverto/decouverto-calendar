9c9c2a08d084f09fcd30005098e702901f256d68
Cédric JUNG <cedced19@gmail.com>
2019-06-22 13:08:38 +0200

    Show which event is old

--- public/javascripts/admin/controllers/list-events.js
+++ public/javascripts/admin/controllers/list-events.js
@@ -1,7 +1,15 @@
 module.exports = ['$scope', '$http', '$rootScope', 'notie', '$location', function ($scope, $http, $rootScope, notie, $location) {
     
     $http.get('/api/events').success(function(events) {
+        for(var i=0; i<events.length; i++) {
+            var start = (new Date(events[i].start)).getTime();
+            var now = (new Date()).getTime();
+           
+            events[i].old = (start < now);
+        }
+
         $scope.events=events;
+
     }).error($rootScope.$error);
 
     $scope.removeEvent = function (id) {
