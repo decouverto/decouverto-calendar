ba58403e2296411ca79decd0c5eb6e69b0b0008e
Cédric JUNG <cedced19@gmail.com>
2023-09-01 14:28:21 +0200

    Add ics file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var ics = require('ics');
 var connections = require('../lib/connections.js');
 var Events = connections.Events;
 
@@ -46,6 +46,51 @@ router.get('/', function (req, res) {
         res.render('index');
     });
     
+});
+
+
+/**
+ * Create ICS file for calendar apps
+ */
+function dateForICS(rawDate) {
+    var date = new Date(rawDate);
+    return [date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(), date.getMinutes()]
+}
+router.get('/ics-file/:id', function(req, res, next) {
+    Events.findById(req.params.id, function(err, event) {
+        if (err) next(err);
+
+        var obj = {
+            start: dateForICS(event.start),
+            title: event.title,
+            description: 'Vous avez été inscrits de l\'événement "' + event.title + '". Plus d’information sur https://calendrier.decouverto.fr/#/' + event.id,
+            url: 'https://calendrier.decouverto.fr/#/' + event.id,
+            status: 'CONFIRMED'
+        }
+
+        if (event.is_located) {
+            obj.location = event.location.name + ' (lat: ' + event.location.lat + ', long: ' + event.location.long + ')';
+            obj.description = obj.description + ' Localisation: ' + obj.location;
+            obj.geo = { lat: event.location.lat, lon: event.location.long };
+        } 
+
+        if (event.is_defined_end) {
+            obj.end = dateForICS(event.end);
+        } else {
+            replacementEnd = new Date(event.start);
+            replacementEnd.setHours(23);
+            replacementEnd.setMinutes(59);
+            obj.end = dateForICS(replacementEnd);
+        }
+
+        ics.createEvent(obj, (error, value) => {
+            if (error) return next(error);
+            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Content-Disposition', `attachment; filename="evenement_decouverto.ics"`);
+            res.send(value);
+        });
+        
+    });
 });
 
 module.exports = router;
